8299b1f25642cf1d1b3586b5f13fb4fccbfbda0f
Lee SJ <elif.dev.kr@gmail.com>
2024-05-20 12:06:45 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,13 +8,13 @@ int main()
     const std::vector<int> referenceString = { 1, 2, 3, 2, 1, 5, 2, 1, 6, 2, 5, 6, 3, 1, 3, 6, 1, 2, 4, 3 };
     constexpr size_t frameSize = 3;
 
-    // 참조 비트 알고리즘
+    // Reference Bit Algorithm
     std::cout << "[Reference Bit Algorithm]\n";
     ReferenceBitAlgorithm(referenceString, frameSize);
 
     std::cout << "\n----------------------------------------\n\n";
 
-    // 시계 알고리즘
+    // Clock Algorithm
     std::cout << "[Clock Algorithm]\n";
     ClockAlgorithm(referenceString, frameSize);
 
